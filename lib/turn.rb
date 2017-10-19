21591693592044b3ef1bd4edce595ec25256acfb
Zach Jensen <zjensen09@yahoo.com>
2017-10-19 15:52:27 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,7 @@
+def display_board(xo)
+  puts " #{xo[0]} | #{xo[1]} | #{xo[2]} "
+  puts "-----------"
+  puts " #{xo[3]} | #{xo[4]} | #{xo[5]} "
+  puts "-----------"
+  puts " #{xo[6]} | #{xo[7]} | #{xo[8]} "
+end
